2bb514a762edb87bcd7e447a09af6d3de9807a05
simpleParadox <abomicode@gmail.com>
2023-03-27 15:55:23 -0600

    Complete adding hyperparameters to gradio app.

--- README.md
+++ README.md
@@ -4,6 +4,40 @@ In this project, we apply different clustering algorithms to an Amazon review da
 The dataset is available at [here](https://www.kaggle.com/datasets/yasserh/amazon-product-reviews-dataset).
 
 ## Goal
+The goal of this project is to apply various clustering algorithms to different embedding types and compare their performance.
+
+What motivated this project?
+Customer reviews are important for businesses. They help businesses understand their customers and improve their products. However, there are instances, 
+where the ratings of the amazon reviews do not reflect the actual sentiment of the review text. For example, a review with a rating of 2 may actually have a review text that might
+be better suited for a 4 star rating. 
+In this project, we choose to look at a question that is a prerequisite to this problem. We ask the question, "What types of embeddings can we use to best represent the 
+review text, and how does the choice of embedding affect the performance of various clustering algorithms?"
+
+
+## Run the experiments.
+To run the experiments, you need to have the following packages installed:
+- numpy
+- pandas
+- sklearn
+- gensim
+- gradio (v3.23.0)
+- matplotlib
+- seaborn
+- torch
+
+Please install these packages according to your operating system and 
+package manager.
+
+This project was built using anaconda.
+
+
+
+## Demo
+To run the demo on your local machine, run the following command:
+```
+python gradio_app.py
+```
+
 
 ## Framework
 
@@ -14,3 +48,5 @@ The dataset is available at [here](https://www.kaggle.com/datasets/yasserh/amazo
 ## Results
 
 ## Conclusion
+
+
--- gradio_app.py
+++ gradio_app.py
@@ -1,32 +1,124 @@
 import gradio as gr
 import numpy as np
 
+from preprocess import load_data, store_embeddings
+from models import Clustering
+
+# General python data science packages.
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.manifold import TSNE
+
+
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
+from sklearn.cluster import OPTICS
+
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import adjusted_rand_score
+
+def load_review_and_ratings():
+    # Load the data
+    reviews, ratings = load_data()
+
+    # Get the indices where the ratings are not nan.
+    good_indices = np.where(np.isnan(ratings) == False)[0]
+    ratings = np.array(ratings)[good_indices]
+    ratings -= 1
+
+    return reviews, ratings, good_indices
 
 
 
-
-
-
-def run_clustering(embedding_type, clustering_algorithm):
+def run_clustering(embedding_type, clustering_algorithm, seed, n_clusters, eps, min_samples, metric):
     # Write a bunch of if else conditions
+    print("Running clustering")
+    print("Number of clusters: ", n_clusters)
+    n_clusters = int(n_clusters)
 
-    r1 = np.random.randint(0, 1000, size=1)
-    return r1, r1 + 1  # The returned value is rendered as a string.
+    reviews, ratings, good_indices = load_review_and_ratings()
 
+    if n_clusters == 3:
+        # Relabel the ratings if the number of selected clusters is 3, else leave them unchanged.
+        ratings[ratings < 3] = 0
+        ratings[ratings == 3] = 1
+        ratings[ratings > 3] = 2
+
+
+    embedding = None
+    model = None
+
+    if embedding_type == 'Word2Vec - Average':
+        embedding = np.load(f"embeds/w2v_embeddings.npz")['arr_0']
+        embedding = embedding[good_indices]
+
+    elif embedding_type == 'BERT - Average':
+        embedding = np.load(f"embeds/bert_avg.npz")['arr_0']
+        embedding = embedding[good_indices]
+
+    elif embedding_type == 'BERT - CLS':
+        embedding = np.load(f"embeds/bert_embeddings.npz")['arr_0']
+        embedding = embedding[good_indices]
+
+    assert embedding is not None
+
+    if clustering_algorithm == 'KMeans':
+        model = KMeans(n_clusters=n_clusters, random_state=seed)
+        model.fit(embedding)
+
+    elif clustering_algorithm == 'Agglomerative Hierarchical':
+        model = AgglomerativeClustering(n_clusters=n_clusters)
+        model.fit(embedding)
+
+    elif clustering_algorithm == 'DBSCAN':
+        model = DBSCAN(eps=eps, min_samples=min_samples, metric=metric)
+        model.fit(embedding)
+
+    elif clustering_algorithm == 'OPTICS':
+        model = OPTICS(eps=eps, min_samples=min_samples, metric=metric)
+        model.fit(embedding)
+
+
+    # Evaluate the model.
+    assert model is not None
+    silhouette = silhouette_score(embedding, model.labels_)
+    ari = adjusted_rand_score(ratings, model.labels_)
+
+    return silhouette, ari
 
 
 with gr.Blocks(theme=gr.themes.Soft()) as demo:
+    gr.HTML('<h1>Choose Embedding type and Clustering Algorithm</h1>')
     with gr.Row():
         with gr.Column():
             embedding_type = gr.Radio(['Word2Vec - Average', 'BERT - Average', 'BERT - CLS'], label='Embedding type', info='Choose the embedding type.')
-            clustering_algorithm = gr.Radio(['KMeans', 'Agglomerative Hierarchical', 'DBSCAN', 'OPTICS'], label='Clustering Algorithm',
-                            info='Choose the clustering algorithm.')
         with gr.Column():
-            silhouette_score = gr.Textbox(label='Silhouette Score')
-            adjusted_rand_score = gr.Textbox(label='Adjusted Rand Score')
+            clustering_algorithm = gr.Radio(['KMeans', 'Agglomerative Hierarchical', 'DBSCAN', 'OPTICS'], label='Clustering Algorithm', info='Choose the clustering algorithm.')
+    gr.HTML('<h1>Choose Hyperparameters</h1>')
+    with gr.Row():
+        with gr.Column():
+            seed = gr.Number(value=42, label='Seed', info='Choose the seed for the clustering algorithm.')
+            n_clusters = gr.Dropdown(['3', '5'], label='Number of clusters', info='Choose the number of clusters.')
+        with gr.Column():
+            eps = gr.Number(value=0.5, label='Epsilon', info='Choose the epsilon for Density based algorithms.')
+            min_samples = gr.Number(value=5, label='Min Samples', info='Choose the min samples for Density based algorithms.',)
+    with gr.Row():
+        metric = gr.Dropdown(['euclidean', 'manhattan', 'cosine'], label='Metric', info='Choose the metric for the clustering algorithm.', value='cosine')
+    gr.HTML('<h1>Results</h1>')
+    with gr.Row():
+        with gr.Column():
+            silhouette = gr.Textbox(label='Silhouette Score')
+        with gr.Column():
+            adjusted_rand = gr.Textbox(label='Adjusted Rand Score')
 
     btn = gr.Button('Run clustering')
-    btn.click(fn=run_clustering, inputs=[embedding_type, clustering_algorithm], outputs=[silhouette_score, adjusted_rand_score])
+    btn.click(fn=run_clustering, inputs=[embedding_type, clustering_algorithm, seed, n_clusters, eps, min_samples, metric], outputs=[silhouette, adjusted_rand])
 
 
 demo.launch()
